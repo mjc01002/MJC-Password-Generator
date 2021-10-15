ed981cb9f01458a2d6802fe4c2e04e6a2b8086f6
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-10-15 14:34:41 -0400

    got a string for a password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,96 +1,108 @@
 // Assignment code here
-var specialChar = ["!","#","$","%","&"];
-// enter values, check password criteria, and validate criteria
+var length = getPasswordLength();
+const numbersList = "0123456789";
+const upperCase2 = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
+const lowerCase3 = "abcdefghilklmnopqrstuvwxyz";
+const specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
-document.querySelector('#generate').addEventListener("click", password); {
-function password(){
-      var passwordLength = window.prompt("Enter a length (8 to 128) for your password.");
-      console.log(passwordLength);
-      if(8 <= passwordLength && passwordLength <= 128){
-        passwordLength = passwordLength;
-      } else {
-        confirm("Please enter a number between 8 and 128!");
-        password();
-      }
+function getPasswordLength() {
+  var length = parseInt(prompt("What length would you like for the password? (min: 8, max: 128)"));
+
+  while (length < 8 || length > 128) {
+   alert("Invalid length. Re-enter length.");
+   length = parseInt(prompt("What length would you like for the password? (min: 8, max: 128)"));
+  }
+
+  return length;
+}
+console.log(length);
+
+
+    var numbers = prompt("Do you want numbers in your password?");
+    var numbers = numbers.toLowerCase()
+    if(numbers === "yes") {
+      numbers = 1;
     
-      window.alert("Password must include at least 1 upperchase, lowercase, or special character.")
-     
+      } else {
+      numbers = 0;
+}
+      console.log(numbers);
       var upperCase = window.prompt("Do you want uppercase letters in your password?");
       var upperCase1 = upperCase.toLowerCase()
       
       if(upperCase1 === "yes") {
-        upperCase1 = passwordLength / 4;
-        upperCase1 = Math.round(upperCase1);
-        
-    } else {
+        upperCase1 = 1;
+          
+        } else {
       upperCase1 = 0;
-    }
-      
-      console.log(upperCase1);
+}
+    console.log(upperCase1);
   
-      var lowerCase = window.prompt("Do you want lower case letters in your password?");
+      var lowerCase = prompt("Do you want lower case letters in your password?");
       var lowerCase1= lowerCase.toLowerCase();
       if(lowerCase1 === "yes") {
-        lowerCase1 = passwordLength / 4;
-        lowerCase1 = Math.round(lowerCase1);
-      } else {
-        lowerCase1 = 0;
-      }
-      console.log(lowerCase1);
+          lowerCase1 = 1;
+          
+        } else {
+          lowerCase1 = 0;
 
-      var specialCharacters = window.prompt("Do you want special characters in your password?");
+    }
+    console.log(lowerCase1);
+  
+      var specialCharacters = prompt("Do you want special characters in your password?");
       var specialCharacters1 = specialCharacters.toLowerCase();
       if(specialCharacters1 === "yes") {
-        specialCharacters1 = passwordLength / 4;
-        specialCharacters1 = Math.round(specialCharacters1);
+        specialCharacters1 = 1;
+    
       } else {
         specialCharacters1 = 0;
       }
+
       console.log(specialCharacters1);
 
-      var upperCase1 = function generateRandomLetter() {
-        for (let i = 0; i < upperCase1.length; i++) { 
-        const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        return alphabet[Math.floor(Math.random() * alphabet.length)]}
-        
-        console.log(upperCase1);
-      //}
-    } } 
-}
 
+    var password = [];
+    let i = 0;
+    while (i < length) {
+    password.push(Math.round(Math.random() * upperCase2.length));
+    i++;
+    var password1 = password.join("");
+    console.log(password1);
+    
+    //console.log(password);
+  
 
- 
- 
+document.getElementById("password").value = password1;
 
+     //while (password.length < length) {
+ //password += //get a random character of the desired types
+   //  } 
 
 // Get references to the #generate element
-//var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-//function writePassword() {
- // var password = generatePassword();
-//  var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password1 = generatePassword();
+  var passwordText = document.querySelector("#password");
 
- // passwordText.value = password;
+  passwordText.value = password1;
 
-//}
-
+}
+      }
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
 
-//function password() {
+generatePassword()
+
   
-//}
+      
+    
 
 
 
 
 
-//function generateRandomLetter1() {
-  //const alphabet = "abcdefghijklmnopqrstuvwxyz"
+//generatePassword()
 
-  //return alphabet[Math.floor(Math.random() * alphabet.length)]
-//}
-
-//console.log(generateRandomLetter1()) // "o"
-//console.log(generateRandomLetter1()) // "X"
+//document.querySelector('#generate').addEventListener("click", getPasswordLength); {
