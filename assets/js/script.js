bb54d19bcab49ca66b6d87d14b1e39cc84207a86
stoncalkins@gmail.com <stoncalkins@gmail.com>
2021-10-15 17:13:46 -0400

    populated empty array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
 // Assignment code here
 var length = getPasswordLength();
-const numbersList = "0123456789";
-const upperCase2 = "ABCDEFGHIJKLMNPQRSTUVWXYZ";
-const lowerCase3 = "abcdefghilklmnopqrstuvwxyz";
-const specialChar = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var characterPool = [];
+const numbersList = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+const upperCase2 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+const lowerCase2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const specialChar = ['!', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+
+
+//document.querySelector('#generate').addEventListener("click", getPasswordLength());
+
+
 
 function getPasswordLength() {
+  
   var length = parseInt(prompt("What length would you like for the password? (min: 8, max: 128)"));
 
   while (length < 8 || length > 128) {
@@ -14,34 +21,39 @@ function getPasswordLength() {
   }
 
   return length;
+  
 }
-console.log(length);
 
+
+function generatePassword() {
 
     var numbers = prompt("Do you want numbers in your password?");
     var numbers = numbers.toLowerCase()
     if(numbers === "yes") {
-      numbers = 1;
+      characterPool.push.apply(characterPool, numbersList);
     
       } else {
       numbers = 0;
 }
       console.log(numbers);
-      var upperCase = window.prompt("Do you want uppercase letters in your password?");
-      var upperCase1 = upperCase.toLowerCase()
+      console.log(characterPool);
       
-      if(upperCase1 === "yes") {
-        upperCase1 = 1;
+      
+    var upperCase = window.prompt("Do you want uppercase letters in your password?");
+    var upperCase1 = upperCase.toLowerCase()
+    if(upperCase1 === "yes") {
+      characterPool.push.apply(characterPool, upperCase2);
           
         } else {
       upperCase1 = 0;
 }
     console.log(upperCase1);
+    console.log(characterPool);
   
       var lowerCase = prompt("Do you want lower case letters in your password?");
       var lowerCase1= lowerCase.toLowerCase();
       if(lowerCase1 === "yes") {
-          lowerCase1 = 1;
+        characterPool.push.apply(characterPool, lowerCase2);
           
         } else {
           lowerCase1 = 0;
@@ -52,15 +64,16 @@ console.log(length);
       var specialCharacters = prompt("Do you want special characters in your password?");
       var specialCharacters1 = specialCharacters.toLowerCase();
       if(specialCharacters1 === "yes") {
-        specialCharacters1 = 1;
+        characterPool.push.apply(characterPool, specialChar);
     
       } else {
         specialCharacters1 = 0;
       }
 
       console.log(specialCharacters1);
-     if( numbers = 0 && upperCase1 = 0 && lowerCase1 = 0 && specialCharacters = 0 ) {
-       
+      console.log(characterPool);
+    // if(numbers = 0 && upperCase1 = 0 && lowerCase1 = 0 && specialCharacters = 0 ) {
+
      }
     
 
